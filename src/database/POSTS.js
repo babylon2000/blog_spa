ef4629ad2f5cf59ba278a70cade584bcbfb9a47b
babylon2000 <kostiantynmakhno@gmail.com>
2018-08-26 22:57:31 +0300

    There is array of objects our posts. Now, I'm adding just one testing object.

---
+++ src/database/POSTS.js
@@ -0,0 +1,11 @@
+const POSTS = [
+    {
+        id: "01kosmkah",
+        author: "Kostiantyn Makhno",
+        text: "That's wonderful life where we live. Nothing else matters.",
+        photo: "https://media.breitbart.com/media/2017/12/wonderfullife-640x480.jpg",
+        date: "26.08.2018"
+    }
+];
+
+export default POSTS;
