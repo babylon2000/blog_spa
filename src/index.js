02c46e50c36e5253112aa035a25b6a0c042af2c2
babylon2000 <kostiantynmakhno@gmail.com>
2018-08-26 14:38:32 +0300

    Main file that call EmojiGreenHeart component.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import EmojiGreenHeart from './hello';
+
+const greeting = <EmojiGreenHeart name="Kostia" />
+
+ReactDOM.render(greeting, document.getElementById('root'));
